5788f6fdd92015e9ee50b91e2de75556d33e83c1
rigojr <rigojunnior@gmail.com>
2018-03-13 09:31:12 -0400

    bootstrap + trabajos en el header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Gruptech Cloud Innovations</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logogtci.png">
+				</div>
+				<div class="col-sm-9">
+					
+				</div>
+			</div>
+		</div>
+	</header>
