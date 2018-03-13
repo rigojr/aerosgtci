220047f9d4e27b03852db0d70f58af7400c1229c
rigojr <rigojunnior@gmail.com>
2018-03-13 11:05:52 -0400

    Trabajos en el header
    Header listo

--- header.php
+++ header.php
@@ -7,17 +7,40 @@
 	<?php wp_head(); ?>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 </head>
 <body>
 	<header>
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logogtci.png">
+				<div class="col-sm-3 logo">
+					<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logogtci.png">
+					</a></p>
 				</div>
-				<div class="col-sm-9">
-					
-				</div>
+				<nav class="col-sm-9 navigation">
+					<ul class="nav justify-content-center">
+  						<li class="nav-item">
+    						<a class="nav-link active" href="#">HOME</a>
+  						</li>
+  						<li class="nav-item">
+    						<a class="nav-link" href="#">SOLUTIONS</a>
+  						</li>
+  						<li class="nav-item">
+   							<a class="nav-link" href="#">NEWS</a>
+  						</li>
+  						<li class="nav-item">
+    						<a class="nav-link" href="#">WHY US</a>
+  						</li>
+  						<li class="nav-item">
+    						<a class="nav-link" href="#">CONTACT</a>
+  						</li>
+  						<li class="nav-item">
+    						<a class="nav-link" href="#">SIGN UP</a>
+  						</li>
+					</ul>
+				</nav>
 			</div>
 		</div>
 	</header>
