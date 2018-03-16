58481684229ec1ebb9b0559b58b3ffffebfd9d9e
rigojr <rigojunnior@gmail.com>
2018-03-15 21:21:08 -0400

    about primeros trabajos

--- custom-about.php
+++ custom-about.php
@@ -4,4 +4,39 @@
 
 <?php get_template_part( 'breadcrumb' ); ?>
 
+<div class="wrap-960">
+	<div class="container">
+		<div class="row info-about">
+			<div class="img-whyus col-sm-4">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logogtci.png" alt="GTCI Logo" title="Gruptech Cloud Innovations" >
+			</div>
+			<div class="about-info-1 col-sm-8">
+				<h2>ABOUT GRUPTECH CLOUD INNOVATIONS</h2>
+				<p>Gruptech Cloud innovations is a new technology company that offers solutionsbased in the cloud, our main headquarter operates from Phoenix, Arizona.</p>
+				<p>Our mission is to bring our customers products and services that offers an innovative change to their Status Quo, making your company more profitable, powerful and also to be the leader in the industry by providing INNOVATIVE, QUALITY and SAFE promotional products to the gifts and premium market.</p>
+				<p class="alineado-text">Gruptech Cloud Innovations vision is to be consolidated as a leading company in solutions based on Cloud Technologies in the Glove.</p>
+			</div>
+		</div>
+		<div class="row info-about">
+			<div class="title-advantages col-sm-4">
+				<h3 class="alineado-text">
+					ADVANTAGES
+				</h3>
+			</div>
+			<div class="about-info-2 col-sm-8">
+				<p>We know the choices We make have significant impacts on people, industry, and environment. That is why we are continually strengthening our commitment to Corporate Responsibility, focusing on the five pillars.</p>
+			</div>
+		</div>
+		<div class="row info-about info-final">
+			<div class="col-sm-12 bia-movement">
+				<h2>SOCIAL RESPONSABILITY</h2>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="wrap-no-960">
+	
+</div>
+
 <?php get_footer(); ?>
