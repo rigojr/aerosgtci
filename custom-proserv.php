3a6ec192be05ee34c67b2ef8b012234d646a8d79
rigojr <rigojunnior@gmail.com>
2018-03-17 23:07:26 -0400

    proserv + breadcrumb del proserv

---
+++ custom-proserv.php
@@ -0,0 +1,91 @@
+<?php /* Template Name: proserv-custom*/ ?>
+
+<?php get_header(); ?>
+
+<?php get_template_part( 'breadcrumb-proserv' ); ?>
+
+<div class="wrap-960 container porserv-info">
+	<div class="row">
+		<div class="col-sm-6 featured-info">
+			<div class="card">
+  				<div class="card-body" >
+    				<h2 class="card-title">SEMC</h2>
+    				<h6 class="card-subtitle mb-2 text-muted">Smart Ethereum Mining Computers</h6>
+    				
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  						<div class="carousel-inner">
+    						<div class="carousel-item active">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/semc.jpg" alt="SEMC on duty image one">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/semc-2.jpg" alt="SEMC on duty image two">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/semc-3.jpg" alt="SEMC on duty image three">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/semc-4.jpg" alt="SEMC on duty image four">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/semc-5.jpg" alt="SEMC on duty image five">
+    						</div>
+  						</div>
+					</div>
+    				<ol>
+    					<li>HASH RATE: 180M/HS</li>
+    					<li>GPUS: 5 GPUS GTX 1070 TI STRIX <b>&</b> 2 GPUS GTX 1070 TI ICX</li>
+    					<li>ELECTRICITY USE: 1.2 KW/H</li>
+    					<li>ROI: 7 – 9 AVG. MONTHS</li>
+    				</ol>
+    				<p class="card-text">
+    					<b>If  your really want to lower your bills simply make money with our SEMC</b>
+    				</p>
+    				<p class="card-text">
+    					MORE INFORMATION...? <a href="#">CLIC HERE</a> 
+    				</p>
+    					<div class="call-contact-btn">
+    						<p><a class="btn btn-success">BOOK YOURS TODAY!</a></p>
+    					</div>
+  				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-6 featured-info">
+			<div class="card">
+  				<div class="card-body" >
+    				<h2 class="card-title">NFC PRODUCTS</h2>
+    				<h6 class="card-subtitle mb-2 text-muted">Badge, Wristband, Coaster & Mouse Pad</h6>
+    				
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  						<div class="carousel-inner">
+    						<div class="carousel-item active">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/nfc-1.jpg" alt="NFC Badge image">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/nfc-2.jpg" alt="NFC Wristband image">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/nfc-3.jpg" alt="NFC Coaster image">
+    						</div>
+    						<div class="carousel-item ">
+      							<img src="<?php echo get_template_directory_uri(); ?>/assets/img/nfc-4.jpg" alt="NFC Mouse Pad image">
+    						</div>
+  						</div>
+					</div>
+    				<ol>
+    					<li><b>NFC BADGE:</b> Material: Velcro backing, Size 50mm dia.</li>
+    					<li><b>NFC WRISTBAND:</b> Material: Silicone</li>
+    					<li><b>NFC COASTER:</b> PU leather, size 90mm dia, NTAG 213 chips</li>
+    					<li><b>NFC MOUSE PAD:</b> Material: Printed paper with PP lamination and 3mm EVA backing.</li>
+    				</ol>
+    				<p class="beau-text"><i>COMING SOON PRODUCTS</i></p>
+    				<div class="call-contact-btn">
+    					<p><a class="btn btn-success">READ MORE</a></p>
+    				</div>
+  				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -11,6 +11,16 @@ function genesis_widgets() {
 		'before_title'  => '',
 		'after_title'   => '',
 	) );
+
+	register_sidebar( array(
+		'name'          => 'Products & Services Submenu Widget',
+		'id'            => 'proserv-submenu',
+		'description'   => 'No desc',
+		'before_widget' => '<div id="%1$s" class="submenu">',
+		'after_widget'  => '</div>',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
 	
 }
 
