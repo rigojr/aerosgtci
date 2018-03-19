fb65a0f5a4265add1eb25b740a259c408ac16045
rigojr <rigojunnior@gmail.com>
2018-03-19 08:29:41 -0400

    trabajo en el slider y otras vergas que viva el desorden

--- front-page.php
+++ front-page.php
@@ -1,22 +1,33 @@
 <?php get_header(); ?>
 
-<center><div class="containerS">
-	<div class="mySlides item-1">
-	  <p>Make your money work for you with a SMART ETHEREUM MINING COMPUTER</p>
-	  <button>BOOK YOURS TODAY</button>
+<div class="container-fluid bg-slider" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/bg-slider.jpg);'">
+	<div class="wrap-slider">
+		<div class="containerS">
+			<div class="mySlides item">
+					<p class="first-call"><b>Make Your Money Work for You</b></p>
+					<p class="second-call"><span>with a</span><b> SMART ETHEREUM MINING COMPUTER</b></p>
+					<p class="third-call call-contact-btn right-call"><a class="btn btn-success" href="#">BOOK YOURS TODAY</a></p>
+			</div>
+			<div class="mySlides item">
+					<p class="first-call"><b>Make Your Money Work for You</b></p>
+					<p class="second-call"><span>with a</span><b> SMART ETHEREUM MINING COMPUTER</b></p>
+					<p class="third-call call-contact-btn center-call"><a class="btn btn-success" href="#">BOOK YOURS TODAY</a></p>
+			</div>
+			<div class="mySlides item">
+					<p class="first-call"><b>Make Your Money Work for You</b></p>
+					<p class="second-call"><span>with a</span><b> SMART ETHEREUM MINING COMPUTER</b></p>
+					<p class="third-call call-contact-btn left-call"><a class="btn btn-success" href="#">BOOK YOURS TODAY</a></p>
+			</div>
+		</div>
+		<div style="text-align:center" class="pagination-slider">
+			<span class="dot" onclick="currentSlide(1)"></span> 
+			<span class="dot" onclick="currentSlide(2)"></span> 
+			<span class="dot" onclick="currentSlide(3)"></span> 
+		</div>
 	</div>
-
-	<p class="mySlides item-2">You take the blue pill - the story ends, you wake up in your bed and believe whatever you want to believe.</p>
-
-	<p class="mySlides item-3">You take the red pill - you stay in Wonderland and I show you how deep the rabbit-hole goes.</p>
-</div></center>
-
-
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
 </div>
+
+
 
 
 <?php get_footer(); ?>
